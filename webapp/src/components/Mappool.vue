dec09971d1d5ccece9f7b4ab411a4d10ba58ac4e
nobishino <nobishii11@gmail.com>
2020-05-10 13:41:42 +0900

    Implement map kick button and websocket client
    - Taro
    - nobishino
    - Refactor
    - UtopianF

--- webapp/src/components/Mappool.vue
+++ webapp/src/components/Mappool.vue
@@ -1,27 +1,51 @@
 <template>
-  <ul class="mappool">
-    <li v-for="map in mappool" v-bind:key="map">
-      <b>{{map}}</b>
-    </li>
-  </ul>
+  <div class="mappool">
+    <div v-for="map in mappool" v-bind:key="map">
+      <input type="checkbox" :id="map" :value="map" v-model="checkedMaps" />
+      <label :for="map">{{map}}</label>
+    </div>
+    <br />
+    <span>Checked Maps: {{ checkedMaps }}</span>
+    <br />
+    <button v-on:click="kickMap">Kick!</button>
+  </div>
 </template>
 
 <script>
 import axios from "axios";
+import Vue from "vue";
 
-export default {
+export default Vue.component("Mappool", {
   name: "Mappool",
-  data() {
-    return {
-      mappool: []
-    };
-  },
-  mounted() {
+  data: () => ({
+    mappool: [],
+    checkedMaps: [],
+    socket: null,
+    url: "ws://localhost:8080/join"
+  }),
+  mounted: function() {
+    this.socket = new WebSocket(this.url);
     axios.get(`${process.env.VUE_APP_API_URL}/api/mappool`).then(response => {
       return (this.mappool = response.data);
     });
+
+    this.socket.onclose = () => {
+      console.log(`Websocket connection to ${this.url} has been closed.`);
+    };
+    this.socket.onopen = () => {
+      console.log(`Websocket connection to ${this.url} has opened.`);
+    };
+    this.socket.onerror = () => {
+      console.error(`Websocket connection to ${this.url} has an error.`);
+    };
+  },
+  methods: {
+    kickMap: function() {
+      console.log(this.checkedMaps);
+      this.socket.send(`I kick ${this.checkedMaps}`);
+    }
   }
-};
+});
 </script>
 
 <style scoped>
